985649c9f064765c9eb892fd3316e1e7cd7b34f8
changyu.che <changyu.che@cue.group>
2020-11-13 16:14:51 +0800

    Initial Commit

---
+++ pages/son/son.js
@@ -0,0 +1,67 @@
+// pages/son/son.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    couponList: [{"name":"二狗", "level":"太孙儿", "coupon_status":1, "info":"吃饭，放屁，逼逼赖赖！", "avatar":"/images/ergou.jpg"},
+                  {"name":"菲儿", "level":"太孙儿", "coupon_status":1, "info":"尿酸，药肝，逼逼赖赖！", "avatar":"/images/feier.jpg"},
+                  {"name":"雷儿", "level":"太孙儿", "coupon_status":1, "info":"长脸，大下巴，逼逼赖赖！", "avatar":"/images/leier.jpg"},]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
